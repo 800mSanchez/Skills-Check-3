38558ac5b81f10191cdd66fa356b91c2259711b7
Alejandro Sanchez <alejandrosanchez9494@gmail.com>
2020-08-14 12:29:58 -0500

    Backend Complete

---
+++ server/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+const app = express();
+
+const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT} = process.env;
+const ctrl = require('./controller');
+
+app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 48},
+    secret: SESSION_SECRET
+}))
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    }).then( db => {
+        app.set('db', db)
+        console.log('connected to db')
+    }).catch( err => console.log(err))
+
+app.post('/auth/login', ctrl.login)
+app.post('/auth/register', ctrl.register)
+app.get('/auth/logout', ctrl.logout)
+app.get('/auth/user', ctrl.getUser)
+
+
+app.listen(SERVER_PORT, ()=> console.log(`Connected to port ${SERVER_PORT}`))
