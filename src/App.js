068239d45737cff2c24cf5e42f6fbec91834bafa
Alejandro Sanchez <alejandrosanchez9494@gmail.com>
2020-08-14 13:48:22 -0500

    Front end/Router/Reducer/Store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import './App.css';
-import Dashboard from './Components/Dashboard/Dashboard';
+/* import Dashboard from './Components/Dashboard/Dashboard';
 import Auth from './Components/Auth/Auth';
 import Form from './Components/Form/Form';
 import Post from './Components/Post/Post';
 import Nav from './Components/Nav/Nav';
+import { render } from '@testing-library/react'; */
+import routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <Auth/>
-      <Dashboard/>
-      <Form/>
-      <Post/>
-      <Nav/>
+    return <div>
+      {routes}
     </div>
-  );
+  
 }
 
 export default App;
--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,12 +1,80 @@
 import React from 'react';
+import axios from 'axios';
+import {connect} from 'react-redux';
+import {loginUser} from '../../ducks/reducer';
 
 class Auth extends React.Component {
-    render(){
-        return <div>Auth
+    constructor(){
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            newUser: false
+        }
 
-        </div>
+    }
+    
+    toggle = () => {
+        this.setState({
+            newUser: !this.state.newUser
+        })
     }
 
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    login = () => {
+        const {email, password} = this.state;
+        axios.post('/auth/login', {email, password}).then(res => {
+            this.props.loginUser(res.data);
+            this.props.history.push('/front_page')
+        }).catch(err => {
+            console.log(err);
+            alert('Login Failed')
+        })
+    }
+
+    register = () => {
+        const {email, password} = this.state;
+        axios.post('/auth/register', {email, password}).then(res => {
+            this.props.loginUser(res.data);
+            this.props.history.push('/front_page');
+        }).catch(err => {
+            console.log(err);
+            alert('Register Failed')
+        })
+    }
+    
+    render(){
+        return <div className="login">
+                <div className="login-container">
+                    <h1>Howdy</h1>
+                    {!this.state.newUser ?
+                    <div>
+                       <input onChange={e => this.changeHandler(e)} name="email" type="text" value={email} placeholder="Email"/>
+                       <input onChange={e => this.changeHandler(e)} name="password" type="password" value={password} placeholder="Password"/>  
+                    <div className="button">
+                        <button onClick={this.login}>Login</button>
+                        <button onClick={this.toggle}>Sign Up</button>
+                    </div>
+                </div>
+            :
+            <div>
+                <input onChange={e => this.changeHandler(e)} name="email" type="text" value={email} placeholder="Email"/>
+                <input onChange={e => this.changeHandler(e)} name="password" type="password" value={password} placeholder="Password"/>
+                <div className="btn-container">
+                    <button onClick={this.register}>Register</button>
+                </div>
+        </div>
+    }
+    </div>
+   </div> 
+}
 }
 
-export default Auth;
+const mapStateToProps = state => state;
+
+export default connect (mapStateToProps, {loginUser})(Auth);
